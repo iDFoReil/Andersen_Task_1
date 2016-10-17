6b3a248b0275e6ac9068fe5dfb3cdd61fc1cb083
iDFoReil <dfor2010@yandex.ru>
2016-10-17 21:38:36 +0300

    changed_v0.1

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ var receptList = document.querySelector('#recepts');
 var buttonObj = document.getElementsByTagName('button');
 
 var recepts = [
-{name: 'a+b', ingredients: ['a','b']},
+{name:'a+b',ingredients: ['a','b']},
 {name:'b+c',ingredients:['b','c']},
 {name:'a+c',ingredients:['a','c']}
 ];
@@ -21,39 +21,46 @@ var recepts = [
 items.addEventListener('click', add);
 DropzoneObj.addEventListener('click', remove);
 buttonObj[0].addEventListener('click', crElement);
-buttonObj[1].addEventListener('click', addRecept);
-buttonObj[2].addEventListener('click', checking);
+buttonObj[1].addEventListener('click', crRecept);
+buttonObj[2].addEventListener('click', crafting);
 receptList.addEventListener('mouseover', ChangeColorToRed);
 receptList.addEventListener('mouseout', ChangeColorToWhite);
 })()
 
 function crElement(){
 	if(CrElemName.value && !document.getElementById(CrElemName.value)){
-	var node = document.createTextNode(CrElemName.value);
-	var pElementCreate = document.createElement('p');
-	pElementCreate.appendChild(node);
-	pElementCreate.id = CrElemName.value;
-	pElementCreate.classList.add('elements');
-	items.appendChild(pElementCreate);
-	CrElemName.value = '';
+		var node = document.createTextNode(CrElemName.value);
+		var pElementCreate = document.createElement('p');
+		pElementCreate.appendChild(node);
+		pElementCreate.id = CrElemName.value;
+		pElementCreate.classList.add('elements');
+		items.appendChild(pElementCreate);
+		CrElemName.value ='';
 	}
-	else{alert('Такой элемент уже существует или название не корректно');}
+	else{
+		alert('Такой элемент уже существует или название не корректно');
+	}
 }
-function addRecept(){
+function crRecept(){
 	var first = document.querySelector('#firstItem').value;
 	var second = document.querySelector('#secondItem').value;
-	var q = { 'name': first+'+'+second, 'ingredients': [first,second]};
-	recepts.push(q);
-	var text = q.name;
-	var pElementCreate = document.createElement('p');
-	var node = document.createTextNode(text);
-	pElementCreate.appendChild(node);
-	receptList.appendChild(pElementCreate);
-	pElementCreate.id = text;
-	pElementCreate.classList.add('elements');
+	if(document.getElementById(first+'+'+second) == null){
+		var q = { 'name': first+'+'+second, 'ingredients': [first,second]};
+		recepts.push(q);
+		var text = q.name;
+		var pElementCreate = document.createElement('p');
+		var node = document.createTextNode(text);
+		pElementCreate.appendChild(node);
+		receptList.appendChild(pElementCreate);
+		pElementCreate.id = text;
+		pElementCreate.classList.add('elements');
+	}
+	else{
+		alert('Такой рецепт уже существует или название не корректно');
+	}
 }
 
-function checking(){
+function crafting(){
 	var pSearchingElement = DropzoneObj.getElementsByTagName('p');
 	if(pSearchingElement.length == 2){
 		var pElements = DropzoneObj.querySelectorAll('p');
@@ -61,12 +68,15 @@ function checking(){
 			if((pElements[0].id == recepts[i].ingredients[0] || pElements[0].id == recepts[i].ingredients[1]) && 
 				(pElements[1].id == recepts[i].ingredients[0] || pElements[1].id == recepts[i].ingredients[1])){
 				document.getElementById(recepts[i].name).style.backgroundColor = 'green';
+			document.getElementById(recepts[i].name).style.color = 'white';
 				DropzoneObj.removeChild(document.getElementById(recepts[i].ingredients[0]));
 				DropzoneObj.removeChild(document.getElementById(recepts[i].ingredients[1]));
 			}
 		}
 	}
-	else{alert('Крафтить можно только из двух элементов')}
+	else{
+		alert('Крафтить можно только из двух элементов');
+	}
 }
 
 function add(event){
@@ -90,7 +100,9 @@ for(var i = 0; i<recepts.length;i++){
 		var elem2 = document.getElementById(recepts[i].ingredients[1]);
 		if(elem1 && elem2){
 			document.getElementById(recepts[i].ingredients[0]).style.backgroundColor= 'red';
+			document.getElementById(recepts[i].ingredients[0]).style.color= 'white';
 			document.getElementById(recepts[i].ingredients[1]).style.backgroundColor= 'red';
+			document.getElementById(recepts[i].ingredients[1]).style.color= 'white';
 		}
 	}
 }
@@ -103,7 +115,9 @@ for(var i = 0; i<recepts.length;i++){
 		var elem2 = document.getElementById(recepts[i].ingredients[1]);
 		if(elem1 && elem2){
 			document.getElementById(recepts[i].ingredients[0]).style.backgroundColor = 'white';
-			document.getElementById(recepts[i].ingredients[1]).style.backgroundColor = 'white';
+			document.getElementById(recepts[i].ingredients[0]).style.color= 'black';
+			document.getElementById(recepts[i].ingredients[1]).style.backgroundColor = 'white'
+			document.getElementById(recepts[i].ingredients[1]).style.color= 'black';
 		}
 	}
 }
